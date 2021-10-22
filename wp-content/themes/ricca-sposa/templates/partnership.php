d2600b2b1b3d272c5b745261fdd7cc2316318765
DESKTOP-K85RN11\Anton <m.onotimerformat@gmail.com>
2021-10-22 23:02:23 +0300

    Обновления стилей

--- wp-content/themes/ricca-sposa/templates/partnership.php
+++ wp-content/themes/ricca-sposa/templates/partnership.php
@@ -19,18 +19,25 @@
                 RiccaSposa on the best terms, we invite you to become our partner. We are always open for cooperation and strive to make this cooperation with RiccaSposa maximum profitable.
                 Experience of many years has allowed us to create advantageous terms of cooperation for customers.
             </p>
-        <form action="partnership.php" metod="post" name="partnershipBtn" class="partnershipbBtn-block">
-            <div>
+        <form action="partnership.php" metod="post" name="partnershipBtn" class="partnershipbBtn-form">
+            <div class="partnership-btn_block">
                 <button type="submit" class="partnership-btn">Partnership</button>
-                <div class="partnership-btn_plus"><img src="<?= get_template_directory_uri();?>/img/plus.svg"" alt=""></div>
+                <div class="partnership-btn_plus"><img src="<?= get_template_directory_uri();?>/img/plus.svg" alt=""></div>
             </div>
-            <div>
+            <div class="partnership-btn_block">
                 <button type="submit" class="partnership-btn">exlusive Partnership</button>
+                <div class="partnership-btn_plus"><img src="<?= get_template_directory_uri();?>/img/plus.svg" alt=""></div>
             </div> 
-            <div>
+            <div class="partnership-btn_block">
                 <button type="submit" class="partnership-btn">franchise</button>
+                <div class="partnership-btn_plus"><img src="<?= get_template_directory_uri();?>/img/plus.svg" alt=""></div>
             </div>
         </form>
+        <div class="partnership-line"></div>
+    </section>
+
+    <section class="partnership-form">
+        <h2>PLEASE FILL IN THIS CONTACT FORM AND WE WILL SEND YOU WHOLESALE PRICE LIST</h2>
     </section>
 
 <?php get_footer();?>
